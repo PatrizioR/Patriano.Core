16741024d6fbe34a8148ccbc0c04a3be7549a0d1
Patrizio Ricciardello <pricciardello@gmail.com>
2021-01-03 20:56:10 +0100

    Add project files.

---
+++ Patriano.Core/Extensions/StringExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patriano.Core.Extensions
+{
+    public static class StringExtension
+    {
+        public static bool IsNullOrEmpty(this string input) => string.IsNullOrEmpty(input);
+
+        public static bool IsNullOrWhiteSpace(this string input) => string.IsNullOrWhiteSpace(input);
+    }
+}
